6aba5995cee0e9acd8bf21ab2b929c162fec6498
Sergio Shambir <realistic.sergio@gmail.com>
2026-01-11 19:31:28 +0100

    fixed-2

--- js/task-2.js
+++ js/task-2.js
@@ -12,7 +12,7 @@ function formatMessage(message, maxLength) {
         return message
     } else {
        return message.slice(0, maxLength) + "..."
-    }
+   }
 }
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
